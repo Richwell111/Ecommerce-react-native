1261d70ab1fca0eb894824db2aba9c51131da946
Richwell111 <bcict20103@gmail.com>
2026-05-23 10:49:26 +0000

    feat: initialize root layout with Clerk, Cart, and Wishlist providers

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,8 +1,8 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { tokenCache } from '@clerk/expo/token-cache'
-// import { CartProvider } from "@/context/CartContext";
-// import { WishlistProvider } from "@/context/WishlistContext";
+import { CartProvider } from "@/context/CartContext";
+import { WishlistProvider } from "@/context/WishlistContext";
 import { ClerkProvider } from '@clerk/expo'
 
 import Toast from 'react-native-toast-message';
@@ -17,10 +17,12 @@ export default function RootLayout() {
   return (
      <GestureHandlerRootView style={{ flex: 1 }}>
             <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-                
+                <CartProvider>
+                    <WishlistProvider>
                         <Stack screenOptions={{ headerShown: false }} />
                         <Toast />
-   
+                    </WishlistProvider>
+                </CartProvider>
             </ClerkProvider>
         </GestureHandlerRootView>
   )
