df09caf2d7ba78af5bf983c7b8940d080872835b
Richwell111 <bcict20103@gmail.com>
2026-05-23 10:48:58 +0000

    feat: implement address management features and core navigation screens for checkout and order history

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -9,10 +9,10 @@ import Toast from 'react-native-toast-message';
 import "@/global.css";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 console.log("DEBUG - Clerk Key:", publishableKey);
-// if (!publishableKey) {
-//   console.error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in environment variables.");
-//   throw new Error('Add your Clerk Publishable Key to the .env file')
-// }
+if (!publishableKey) {
+  console.error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in environment variables.");
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 export default function RootLayout() {
   return (
      <GestureHandlerRootView style={{ flex: 1 }}>
